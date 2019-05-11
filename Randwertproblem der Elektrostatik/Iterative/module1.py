3d87b383648488e986d286aeea8a39a7851d3e6d
Maximilian Köper <M.Koeper@gmx.net>
2019-05-11 13:56:44 +0200

    Sync to master

--- Randwertproblem der Elektrostatik/Iterative/module1.py
+++ Randwertproblem der Elektrostatik/Iterative/module1.py
@@ -2,18 +2,26 @@
 
 #Berechnung wird von beiden Seiten vorgenommen um die Konvergenzgeschwindigkeit zu erhöhen
 #Blöcke mit den Werten 0,100,-100 kV werden nicht überschrieben
+import copy
 
-def iterativeLaplace(V,d):
-    x=0
+def iterativeLaplace(V):
+    U = V.copy()
+    total_error = float(0)
     print("\n[ ] Iteration wird durchgeführt")
+    x = True
+    iteration_count = 0
+    o = 0
 
-    while x < d/2:
+    while x == True:
+        o = 0
+        iteration_count += 1
         i = 0
+        total_error = 0
         while i<50:
             j=0
             while j<50:
                 if V[i][j] !=0 and V[i][j] != 100 and V[i][j] !=-100:
-                    V[i][j]=(V[i-1][j]+V[i+1][j]+V[i][j-1]+V[i][j+1])/4
+                    V[i][j]=float((V[i-1][j]+V[i+1][j]+V[i][j-1]+V[i][j+1])/4)
                 j +=1
             i += 1
         i=50
@@ -21,8 +29,23 @@ def iterativeLaplace(V,d):
             j=50
             while j>0:
                 if V[i][j] !=0 and V[i][j] != 100 and V[i][j] !=-100:
-                    V[i][j]=(V[i-1][j]+V[i+1][j]+V[i][j-1]+V[i][j+1])/4
+                    V[i][j]=float((V[i-1][j]+V[i+1][j]+V[i][j-1]+V[i][j+1])/4)
                 j -=1
             i -= 1
-        x += 1
-    return V
+        #Berechnung des Fehlers
+        i = 0
+        while i<50:
+            j=0
+            while j<50:
+                total_error += abs(float(V[i][j] - U[i][j]))
+                o += 1
+                j +=1
+            i += 1
+        if total_error < 20:
+            x = False
+        U = V.copy()
+
+
+
+    total_error_mean = float(total_error/o)
+    return V, iteration_count, total_error, total_error_mean
--- Randwertproblem der Elektrostatik/Randwertproblem_der_Elektrostatik.py
+++ Randwertproblem der Elektrostatik/Randwertproblem_der_Elektrostatik.py
@@ -2,25 +2,27 @@ import Iterative as it
 from array import *
 import random
 from numpy.random import seed
-from numpy.random import rand
+from numpy.random import randint
+import time
 
 # Initialisiere Array für Box
 # Größe 50x50 
 # Innere Größe 100x100
 # Werte werden als float per numpy generiert 
+# Zufallswerte sind zwischen 1 und 99
 
 import matplotlib.pyplot as plt
 import numpy as np
-seed(1)
 x = np.linspace(-11,41, num=52)
 y = np.linspace(-24,26, num=52)
 
-X, Y = np.meshgrid(x,y)
-Z = np.zeros((52,52))
+X, Y = np.meshgrid(x,y, sparse=True ,copy=True)
+Z = np.zeros((52,52), dtype=np.float)
 
 for i in range(52):
    for j in range(52):
-       Z[i,j] = rand()*100
+       #Z[i,j] = rand()*100
+       Z[i,j] = float(randint(1, high=99, size=None))
 
 #DEBUG
 #plt.pcolor(X, Y, Z)
@@ -91,15 +93,28 @@ elif abc == "C":
             j +=1
         j = 38
         i += 1
-else:
+elif abc != "A":
     print("\nERROR")
-    exit(1)
+    #exit(1)
 
+
+#Warte auf Enter
 try:
     input("\n[*] Alle Daten geladen\n\nEnter um Berechnung zu starten")
 except SyntaxError:
     pass
-Z=it.iterativeLaplace(Z,1000)
+
+#Führe Iterationen aus
+a = time.perf_counter()
+Z, iteration_count, total_error, total_error_mean = it.iterativeLaplace(Z)
+b = time.perf_counter()
+
+
+print("\nBenötigte Zeit(in s): " + str((b-a)))
+print("\nBenötigte Iterationen: " + str(iteration_count))
+print("\nGesamter Fehler (letzter Schritt): " + str(total_error))
+print("\nDurchsnittlicher Fehler (letzter Schritt): " + str(total_error_mean))
+#Zeige Ergebnis der Berechnung
 plt.pcolormesh(X, Y, Z)
 plt.show()
 
