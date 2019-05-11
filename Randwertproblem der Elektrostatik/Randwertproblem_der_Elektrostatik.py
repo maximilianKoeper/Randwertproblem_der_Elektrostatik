a57ad635072defb3ac6ddfb4cd4cb4263836e8c0
Maximilian Köper <M.Koeper@gmx.net>
2019-05-11 11:13:03 +0200

    Using np.meshgrid instead of List

--- Randwertproblem der Elektrostatik/Randwertproblem_der_Elektrostatik.py
+++ Randwertproblem der Elektrostatik/Randwertproblem_der_Elektrostatik.py
@@ -6,67 +6,78 @@ from numpy.random import seed
 from numpy.random import rand
 
 # Initialisiere Array für Box
-# Größe 120x120 
+# Größe 50x50 
 # Innere Größe 100x100
 # Werte werden als float per numpy generiert 
 
-T = []
-i = 0
-seed(1)
-while i < 120:
-  B = rand(120)
-  T.insert(i, B)
-  i += 1
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-11,41, num=52)
+y = np.linspace(-24,26, num=52)
+
+X, Y = np.meshgrid(x,y)
+Z = np.zeros((52,52))
+
+for i in range(52):
+   for j in range(52):
+       Z[i,j] = rand()
+
+#DEBUG
+#plt.pcolor(X, Y, Z)
+#plt.show()
 
 print("[*] Array initialisiert \n")
 
 #Initialisiere Äußeren Leiter
-#jeweils 10 Blöcke oben und seitlich
+#jeweils 1 Block oben und seitlich
 #Potential außen = 0*100000V
 #Innere Anordung wird nach User Eingabe initialisiert
 
 i = 0
 B = []
-while i < 120:
+while i < 52:
     B.insert(i, 0)
     i += 1
 
-i = 0
-while i < 10:
-    T[i] = B
-    T[119 - i] = B
-    i += 1
+Z[0] = B
+Z[50] = B
 
 i = 0
-while i < 120:
-    j=0
-    while j < 10:
-        T[i][j] = 0
-        T[i][119-j] = 0
-        j += 1
+while i < 52:
+    Z[i][0] = 0
+    Z[i][50] = 0
     i += 1
-
-print("[*] Äußerer Leiter initialisiert\n")
 
 #DEBUG
-#Zeigt aktuelles Array mit Außenleiter
-#pt.plotlist(T)
+#plt.pcolor(X, Y, Z)
+#plt.show()
+print("[*] Äußerer Leiter initialisiert\n")
 
 #User Abfrage 
 #Welche Szenario soll berechnet werden
 #A/B/C vgl. Aufgabenblatt
 
 input = input("Welche Anordnung soll berechnet werden (A/B/C): ")
+
+#Generiert geladene Fläche (4x4)
+i = 23
+j = 9
+while i < 27:
+    while j < 13:
+        Z[i][j]=1
+        j +=1
+    j = 9
+    i += 1
+#DEBUG
+#plt.pcolor(X, Y, Z)
+#plt.show()
+
+#Szenario wird generiert
 if input == "A":
-    i = 55
-    j = 25
-    while i < 64:
-        while j < 34:
-            T[i][j]=1
-            j +=1
-        j = 25
-        i += 1
+    print("A")
 elif input == "B":
+
     print("B")
 elif input == "C":
     print("C")
@@ -74,6 +85,5 @@ else:
     print("ERROR")
     exit(1)
 
-#DEBUG
-#Zeigt aktuelles Array mit allen Leitern
-pt.plotlist(T)
+
+
